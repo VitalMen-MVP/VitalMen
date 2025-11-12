39bc95acdd22945382bf721a557f5a25f4182e2d
Lucas Magalhães <conceicaolucas68@gmail.com>
2025-11-12 15:23:06 -0300

    Delete VitalMen-main directory

--- VitalMen-main/backend/database/db_config.py
+++
@@ -1,6 +0,0 @@
-# arquivo com dados de conexão do banco de dados
-
-DATABASE_URL = f"postgres://postgres.amgqmjakblyakhjpqxua:HzSDphczguAVSK20@aws-1-sa-east-1.pooler.supabase.com:5432/postgres?sslmode=require"
-
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
